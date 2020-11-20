3718ea2d9ee6221dc27aadfd7557fecc9e2b5351
Jorge <jsacris@hotmail.com>
2020-11-20 21:06:25 +0100

    First review of the digits.cs program.

--- digits.cs
+++ digits.cs
@@ -1,35 +1,49 @@
+//Author: Jorge Sacristan
+
 using System;
 
+//Counters of number of digits,
+//countOneDigit is for one digit, countTwoDigit is for 2 digits
+//countThreeDigit is for 3 digits and countMoreThanThreeDigit is for more than 3 digits
 class digits
 {
- static void Main()
- {
-	 //counters of number of digits a is for one digit, b is for 2 digits, c is for 3 digits and d is for more than 3 digits
-     int a = 0, b = 0, c = 0, d = 0;
-      Console.Write("Insert a number: ");
-      // read the number as string to check the word "end"
-      string e = Console.ReadLine();
-     while(e!="end"){
-int input = Convert.ToInt32(e);
-       if(input != 0)
-{
-       if(input/10 == 0)
-           a ++;  // it only has one digit
-       else 
-       if (input/100 == 0)
-		b ++;  // the number has 2 digits
-        else
-        if (input/1000 == 0)
-        c ++;  // the number has 3 digits
-        else
-           d++;  // the number has more than 3 digits
+    static void Main()
+    {
+        int countOneDigit = 0, countTwoDigit = 0, countThreeDigit = 0, countMoreThanThreeDigit = 0, inputNumberUser = 0;
+        String inputUser = "";
+
+        Console.Write("Insert a number: ");
+        inputUser = Console.ReadLine();     // read the number as string to check the word "end"
+
+        while (inputUser != "end")
+        {
+            inputNumberUser = Convert.ToInt32(e);
+
+            if(inputNumberUser != 0)            
+            {
+                if (inputNumberUser / 10 == 0)
+                {
+                    countOneDigit++;  // it only has one digit
+                }
+                else
+                {
+                    if (inputNumberUser / 100 == 0)
+                    {
+                        countTwoDigit++;  // the number has 2 digits
+                    }
+                    else
+                    {
+                        if (inputNumberUser / 1000 == 0)
+                            countThreeDigit++;  // the number has 3 digits
+                        else
+                            countMoreThanThreeDigit++;  // the number has more than 3 digits
+                    }
+                }
+
+            }
+            inputUser = Console.ReadLine();
         }
-      e = Console.ReadLine();
-}
-
-Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", a, b, c, d); 
-
-
- }
+        Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", countOneDigit, countTwoDigit, countThreeDigit, countMoreThanThreeDigit); 
+    }
 }
 
